723b39826fa75f2a541338870a78552a100835d2
Ram Naik <naikram1986@gmail.com>
2018-09-12 11:19:06 +0300

    added tea or coffe interactive javascript

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var drinks = prompt("would you like to have tea or coffee? answer with 'tea' or 'coffee':")
+if (drinks === "coffee") {
+  console.log(alert("Piping hot coffee comin up!"));
+} else if (drinks === "tea") {
+  console.log(alert("A hot cup of tea comin up!"));
+} else {
+  console.log(alert("ERROR please enter 'tea' or 'coffee'"));
+}
